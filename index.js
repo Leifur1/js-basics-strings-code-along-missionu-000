029d4995d5e15aab109bd7fa88c5726338e83ef5
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-27 21:18:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-let shortGreeting = `Welcome, ${currentUser}`
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}`
