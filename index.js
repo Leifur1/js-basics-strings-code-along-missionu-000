047505fe34ecb9a796018894cdbe35e222e4f761
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-27 20:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let currentUser = "Leifur Sigurdsson";
 
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + uppercase(currentUser) + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase + "!";
